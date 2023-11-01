064d086972e038508d197323f82d36d3fa832e07
Luis Mateo Michel Flores <101907819+Bussy888@users.noreply.github.com>
2023-11-01 19:02:24 -0400

    Commit Final

--- src/test/java/estructuraWebUI/pageTodoLy/MainPage.java
+++ src/test/java/estructuraWebUI/pageTodoLy/MainPage.java
@@ -1,9 +1,11 @@
 package estructuraWebUI.pageTodoLy;
 
+import estructuraAppium.control.TextBox;
 import org.openqa.selenium.By;
 import estructuraWebUI.controls.Button;
 
 public class MainPage {
     public Button loginButton = new Button(By.xpath("//img[@src='/Images/design/pagelogin.png']"));
     public Button signUpButton = new Button(By.xpath("//img[@src='/Images/design/pagesignup.png']"));
+    public TextBox errorSignUp = new TextBox(By.id("ErrorMessageText"));
 }
--- src/test/java/estructuraWebUI/testSuite/TodoLyTest.java
+++ src/test/java/estructuraWebUI/testSuite/TodoLyTest.java
@@ -10,16 +10,19 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.util.Date;
+import java.util.Random;
+
 public class TodoLyTest {
 
     MainPage mainPage = new MainPage();
     SignUpSection signUp = new SignUpSection();
     LoginSection login = new LoginSection();
     CentralSection centralSection = new CentralSection();
-
-
+    Random rand = new Random();
+    public int random = rand.nextInt();
     String name = "Mateo Michel";
-    String email = "mateomichel@examenfinal.com";
+    String email = "mateomichel"+random+"@examenfinal.com";
     String password = "Contra12345";//Pass12345
 
     @AfterEach
@@ -29,7 +32,7 @@ public class TodoLyTest {
 
     @BeforeEach
     public void open(){
-        Session.getInstance().getBrowser().get("https://todoist.com/");
+        Session.getInstance().getBrowser().get("https://todo.ly/");
     }
     @Test
     public void loginTesting() throws InterruptedException {
@@ -44,6 +47,14 @@ public class TodoLyTest {
         Assertions.assertTrue(centralSection.openSettingsButton.isControlDisplayed(), "ERROR no se pudo iniciar sesion");
 
         centralSection.logoutButton.click();
+        mainPage.signUpButton.click();
+        signUp.emailTextbox.setText(email);
+        signUp.passwordTextbox.setText(password);
+        signUp.fullNameTextbox.setText(name);
+        signUp.acceptTermsButton.click();
+        signUp.signUpButton.click();
+        Thread.sleep(2000);
+        Assertions.assertTrue(signUp.signUpButton.isControlDisplayed(), "ERROR No se creo la cuenta anteriormente");
 
     }
 }
